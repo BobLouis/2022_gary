59f165ee99754dcf57576239bde824785780dfd9
Louis <bob900316@gmail.com>
2022-03-30 21:07:41 +0800

    init

---
+++ INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -0,0 +1,2 @@
+1648638946 /home/caid035/2022_univ_cell/tb.sv
+1648640129 /home/caid035/2022_univ_cell/JAM.v
---
+++ INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,4 @@
+1648638656 /home/caid035/2022_univ_cell/tsmc13_neg.v
+1648640573 /home/caid035/2022_univ_cell/tb.sv
+1648640129 /home/caid035/2022_univ_cell/JAM.v
+1648640145 /home/caid035/2022_univ_cell/JAM_syn.v
